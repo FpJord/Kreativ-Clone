3f78e47927df92c7acf990ed8f04e3767991c974
Felipe de Lima Jordão <DUQ0288451@aluno.firjansenaisesi.com.br>
2023-10-24 11:46:46 -0300

    Segunda versão

---
+++ assets/js/main.js
@@ -0,0 +1,31 @@
+function revealPassword () {
+    var inputPass = document.getElementById('senha')
+    var btnShowPass = document.getElementById('btn-senha')
+
+    if(inputPass.type === 'password'){
+        inputPass.setAttribute('type','text')
+        btnShowPass.classList.replace('bi-eye', 'bi-eye-slash')
+    } else{
+        inputPass.setAttribute('type','password')
+        btnShowPass.classList.replace('bi-eye-slash','bi-eye')
+    }
+}
+
+function validate(){
+    var inputPass = document.getElementById('senha')
+    var len = document.getElementById('minimum-characters')
+    var symbol = document.getElementById('one-symbol')
+
+    if(inputPass.value.length > 8){
+        len.style.color='green'
+    } else {
+        len.style.color='red'
+    }
+
+    if(inputPass.value.match(/[!\@\#\$\%\¨\&\*\(\)\-\+\=\/\\\,\.\;\:\?\^\~\{\}\[\]\]]/)){
+        symbol.style.color='green'
+    } else {
+        symbol.style.color='red'
+    }
+}
+       
